63628278c29a7299987e3b9ff1fa9c1e367ba68c
Law, Sean <Sean.Law@tdameritrade.com>
2018-01-02 01:38:25 -0500

    Added callbacks to select/deselect circles

--- subtraction.py
+++ subtraction.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 from bokeh.plotting import figure, curdoc
-from bokeh.models import ColumnDataSource, LabelSet
+from bokeh.models import ColumnDataSource, LabelSet, CustomJS, TapTool
 from bokeh.models.widgets import Button
 from bokeh.models.widgets import RadioButtonGroup
 from bokeh.layouts import widgetbox, column, row, layout
@@ -26,6 +26,7 @@ def generate_data():
 
 	circle_data = {'x': range(high_num),
 				   'y': [1] * high_num,
+				   'selected': [False] * high_num
 				   }
 
 	equation_data = {'x': [-0.5],
@@ -38,14 +39,42 @@ def generate_data():
 
 generate_data()
 
-p = figure(plot_width=1200, plot_height=500, x_range=(-1, 11), y_range=(0,1.5), tools='tap')
+
+taptool_callback = CustomJS(args=dict(cds=circle_cds), code="""
+	var selected_idx = cds.selected['1d'].indices
+	cds.data['selected'][selected_idx] = !cds.data['selected'][selected_idx]
+
+    cds.selected['1d'].indices = []
+    for (i=0; i< cds.data['selected'].length; i++){
+        if (cds.data['selected'][i] == true){
+            cds.selected['1d'].indices.push(i)
+        }
+    }
+	""")
+
+taptool = TapTool(callback=taptool_callback)
+
+p = figure(plot_width=1200, plot_height=500, x_range=(-1, 11), y_range=(0.0,1.5), tools=[taptool])
+p.xaxis.visible = False
+p.xgrid.visible = False
+p.yaxis.visible = False
+p.ygrid.visible = False
 c = p.circle(x='x', y='y', source=circle_cds, size=50,
              selection_color='#1F77B4', 
              selection_line_color=None, 
              selection_fill_alpha=0.25,
              nonselection_fill_alpha=1.0,
             )
-c.data_source.on_change('selected', handler)
+
+# Detect when circles are deselected and reset `selected` column in cds
+circle_cds.callback = CustomJS(code="""
+    if (cb_obj.selected['1d'].indices.length == 0){
+        for (i=0; i< cb_obj.data['selected'].length; i++){
+            cb_obj.data['selected'][i] = false        
+        }
+        console.log(cb_obj.data['selected'])
+    }
+    """)
 
 equation = LabelSet(x='x', y='y', text='text', source=equation_cds, text_font_size='100pt')
 
